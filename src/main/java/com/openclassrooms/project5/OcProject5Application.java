e5c09b8ddb46ff96198444fdfaecf5b8cb26bc23
Antonio Jagodic <ajagodic1990@gmail.com>
2019-10-02 21:24:49 +0200

    My first commit

---
+++ src/main/java/com/openclassrooms/project5/OcProject5Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.project5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcProject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcProject5Application.class, args);
+	}
+
+}
