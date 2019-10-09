87ebe2846d06490beeca0cdd94d19d840fc7c8cd
Antonio Jagodic <ajagodic1990@gmail.com>
2019-10-09 17:46:07 +0200

    Updating some css, html (home and login), fixing WebSecurityConfig

--- src/main/java/com/openclassrooms/project5/config/SecurityConfig.java
+++ src/main/java/com/openclassrooms/project5/config/SecurityConfig.java
@@ -1,36 +1,30 @@
 package com.openclassrooms.project5.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
 				.authorizeRequests()
-				//.antMatchers("/css/**", "/", "/home").permitAll()
-				.antMatchers("/admin/**").hasRole("ADMIN")
-		
-				.and()
-				.formLogin().loginPage("/login").failureUrl("/login-error")
-		
-		     	.and()
-		     	.logout()
-		     	.logoutUrl("/logout")
-		     	.logoutSuccessUrl("/")
-      ;
+                .antMatchers("/**", "/home").permitAll()
+            
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error")
+
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+        ;
     }
 
     @Override
@@ -39,4 +33,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser(User.withDefaultPasswordEncoder().username("admin").password("password").roles("ADMIN"));
     }
+
 }
