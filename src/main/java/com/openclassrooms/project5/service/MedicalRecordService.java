3ca49a3fd5cfc214517a161e032ea90381d28d7c
Antonio Jagodic <ajagodic1990@gmail.com>
2020-01-08 16:39:57 +0100

    Write post, put and delete Person Rest endpoints

--- src/main/java/com/openclassrooms/project5/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/project5/service/MedicalRecordService.java
@@ -2,7 +2,25 @@ package com.openclassrooms.project5.service;
 
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.project5.domain.Firestation;
+import com.openclassrooms.project5.domain.MedicalRecord;
+
 @Service
 public class MedicalRecordService {
-
+	
+	// ---------- ENDPOINTS ----------
+	
+		public MedicalRecord createFirestation(MedicalRecord medicalRecord) {
+			return medicalRecordRepository.createMedicalRecord(medicalRecord);
+		}
+		
+		public MedicalRecord updateFirestation(MedicalRecord medicalRecord) {
+			return medicalRecordRepository.updateMedicalRecord(medicalRecord);
+		}
+		
+		public boolean deleteFirestation(MedicalRecord medicalRecord) {
+			return medicalRecordRepository.deleteMedicalRecord(medicalRecord);
+		}
+		
+		// ---------- END OF ENDPOINTS ----------
 }
